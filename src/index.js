5ab9473a3f7c9f302e3bf7dca603d0a776e2f045
Daniel Trejo <dwtrejo14@comcast.net>
2024-05-20 11:09:37 -0400

    Complete Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+    const form = document.getElementById("create-task-form");
+    const taskList = document.getElementById("tasks")
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+    const newTaskDescription = document.getElementById("new-task-description").value;
+
+    const newTaskItem = document.createElement("li")
+    newTaskItem.textContent = newTaskDescription
+
+    taskList.appendChild(newTaskItem);
+
+    document.getElementById("new-task-description").value = "";
+
+    const deleteButton = document.createElement('button');
+
+    deleteButton.textContent = "X"
+    deleteButton.style.marginLeft = "10px"
+    deleteButton.style.padding = "2px 4px"
+    deleteButton.style.fontSize = "10px"
+
+
+    deleteButton.addEventListener("click", () => {
+    taskList.removeChild(newTaskItem);
+
+
+    })
+    newTaskItem.appendChild(deleteButton);
+
+    })
 });
